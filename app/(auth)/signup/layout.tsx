30e198ba78d76af9d901bacff9859bd472d1ee4a
Safa Pc <safaabdolreza@gmail.com>
2026-02-23 09:11:53 +0330

    open grafh

--- app/(auth)/signup/layout.tsx
+++ app/(auth)/signup/layout.tsx
@@ -12,11 +12,21 @@ export const metadata: Metadata = {
     description:
       "اپلیکیشن دبرنای آنلاین و تورنومنت دبرنای دینگ مانی- بازی کنید، برنده شوید.",
     url: `${siteOrigin}/signup`,
+    images: [
+      {
+        url: "/ding_money_preview.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Dingmoney - بازی آنلاین و تورنومنت دبرنا",
+      },
+    ],
   },
   twitter: {
+    card: "summary_large_image",
     title: "لینک ثبت نام اپلیکیشن دینگ مانی",
     description:
       "اپلیکیشن دبرنای آنلاین و تورنومنت دبرنای دینگ مانی- بازی کنید، برنده شوید.",
+    images: ["/ding_money_preview.jpg"],
   },
 };
 
